36802053b496ce04489b44bd6b0c4ffdf42170d9
rr1980 <rener1980@googlemail.com>
2018-09-21 20:25:40 +0200

    Projektdateien hinzufügen.

---
+++ rrServiceNetV2.GatewayService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rrServiceNetV2.GatewayService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
